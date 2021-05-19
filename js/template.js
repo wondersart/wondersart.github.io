7f343acd0ed74bdac82957a98f6e2dba476ee02e
felix <felix@netopia.ro>
2021-05-19 21:58:56 +0300

    ajax call

--- js/template.js
+++ js/template.js
@@ -3,16 +3,24 @@ $(document).ready( () => {
         "author":"Working Author",
         "title":"Working Title",
         "category":"Work Category",
-        "description":"Very nice!",
-        "content":"Extremely, very nice",
+        "description":"Office ipsum",
+        "content":"Hit the ground running gage [sic] where the industry is heading and give back to the community what we’ve learned where the metal hits the meat viral engagement, nor optics I just wanted to give you a heads-up. Let's put a pin in that shotgun approach, so goalposts, out of scope you must be muted. My capacity is full do i have consent to record this meeting what are the expectations or race without a finish line, yet data-point product management breakout fastworks net net. 4-blocker both the angel on my left shoulder and the devil on my right are eager to go to the next board meeting and say we’re ditching the business model where do we stand on the latest client ask. creativity requires you to murder your children they have downloaded gmail and seems to be working for now nail it down but clear blue water and table the discussion , or work so not enough bandwidth. That's not on the roadmap drive awareness to increase engagement we need to aspirationalise our offerings performance review herding cats, for low engagement this is a no-brainer.",
         "date":"2021-May-17",
-        "image":"http://tavonline.net/html/gorge2/dark/images/portfolio/work-1/01.jpg",
+        "image":"assets/01.jpeg",
         "next":"Next Work",
-        "nexturl":"gallery.html?2"
-    }
+        "nexturl":"gallery.html?token=2"
+    };
+    $.ajax({
+        url:'https://api.wonders.art/v1/token/0',
+        contentType: "application/json",
+    }).done((data)=>{
+        if(data!=null){
+            console.log("resp",data);
+        }
+    })
+    console.log('params',new URLSearchParams(window.location.search));
     $.get('templates/gallery.hbs',  (data) => {
         var template=Handlebars.compile(data);
         $('#wrapper').html(template(jsonData));
-       // $('#wrapper').html('abcd');
     }, 'html');
 });
